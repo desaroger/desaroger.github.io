63d383412fb98f2f35dea5564a15a24e99e77ab7
Roger Fos Soler <desaroger23@gmail.com>
2023-05-09 18:53:29 +0200

    fix: small improvements in organization

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code of https://desaroger.github.io/
--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 ﻿
 document.querySelector("#years").innerHTML = new Date().getFullYear() - 2012;
+
+console.log("⛔ Hacker detected ⛔");
+console.log("✅ hacker.intentions are \"mess around. Curiosity. Check what tech stack has been used here\"");
+console.log("✅ hacker.iq is less than the threshold, it is not a threat");
